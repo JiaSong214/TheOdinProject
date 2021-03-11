1220e3d3cea38ad1224ab9eeab04e8ef9a451665
Jia Song <jiasong214@gmail.com>
2021-03-11 20:58:42 +1100

    feat: Add local storage function

--- to-do_list/script/Model.js
+++ to-do_list/script/Model.js
@@ -1,23 +1,12 @@
 // ((exports) => {
   const Model = (() => {
 
-    function ToDo (title, description, dueDate, priority, checked, project) {
-      this.id = `${todoModel.getData().length}+${new Date().getTime()}`,
-      this.title = title,
-      this.description = description,
-      this.dueDate = dueDate,
-      this.priority = priority,
-      this.checked = checked,
-      this.project = project
-    }
-    
-    function Project (title) {
-      this.id = `${projectModel.getData().length}+${new Date().getTime()}`,
-      this.title = title
-    }
+    //Local Storage
+    const LS = {
+      todoLS: [],
+      projectLS: [],
 
-    const todoModel = {
-      todoObj: [
+      defaultTodo: [
         {
           id: 0,
           title: 'Make a To Do List',
@@ -49,30 +38,22 @@
           id: 3,
           title: 'Study Odin Project',
           description: '',
-          dueDate: '',
+          dueDate: '2021-04-25',
           priority: false,
           checked: false,
           project: 'Study'
+        },
+        {
+          id: 4,
+          title: 'Call paypal',
+          description: '',
+          dueDate: '',
+          priority: true,
+          checked: false,
+          project: ''
         }
       ],
-      getData: () => {
-        return todoModel.todoObj;
-      },
-      addData: (newData) => {
-        const newTodo = new ToDo(...newData);
-        todoModel.todoObj.push(newTodo);
-      },
-      deleteData: (dataId) => {
-        const newTodoObj = todoModel.todoObj.filter(todo => todo.id !== dataId);
-        todoModel.todoObj = newTodoObj;
-      },
-      changeData: (modifiedTodoObj) => {
-        todoModel.todoObj = modifiedTodoObj;
-      }
-    }
-  
-    const projectModel = {
-      projectObj: [
+      defaultProject: [
         {
           id: 0,
           title: 'Study'
@@ -82,19 +63,110 @@
           title: 'Shopping'
         }
       ],
+
+      getTodoLS: function () {
+        return JSON.parse(localStorage.getItem('to-dos'));
+      },
+      setTodoLS: function (todos) {
+        localStorage.setItem('to-dos', JSON.stringify(todos));
+      },
+      getProjectLS: function () {
+        return JSON.parse(localStorage.getItem('projects'));
+      },
+      setProjectLS: function (projects) {
+        localStorage.setItem('projects', JSON.stringify(projects));
+      },
+    }
+
+
+    function ToDo (title, description, dueDate, priority, checked, project) {
+      this.id = `${todoModel.getData().length}+${new Date().getTime()}`,
+      this.title = title,
+      this.description = description,
+      this.dueDate = dueDate,
+      this.priority = priority,
+      this.checked = checked,
+      this.project = project
+    }
+    
+    function Project (title) {
+      this.id = `${projectModel.getData().length}+${new Date().getTime()}`,
+      this.title = title
+    }
+
+
+    const todoModel = {
+      todoObj: [],
+      getData: () => {
+        return todoModel.todoObj;
+      },
+      addData: function (newData) {
+        const newTodo = new ToDo(...newData);
+        todoModel.todoObj.push(newTodo);
+
+        LS.setTodoLS(this.getData());
+      },
+      deleteData: function (dataId) {
+        const newTodoObj = todoModel.todoObj.filter(todo => todo.id !== dataId);
+        todoModel.todoObj = newTodoObj;
+
+        LS.setTodoLS(this.getData());
+      },
+      changeData: function (modifiedTodoObj) {
+        todoModel.todoObj = modifiedTodoObj;
+
+        LS.setTodoLS(this.getData());
+      }
+    }
+
+
+    const projectModel = {
+      projectObj: [],
       getData: () => {
         return projectModel.projectObj;
       },
-      addData: (newTitle) => {
+      addData: function (newTitle) {
         const newProject = new Project(newTitle);
         projectModel.projectObj.push(newProject);
+
+        console.log(this.getData())
+        LS.setProjectLS(this.getData());
       },
-      deleteData: (dataId) => {
+      deleteData: function (dataId) {
         const newProjectObj = projectModel.projectObj.filter(project => project.id !== dataId);
-  
         projectModel.projectObj = newProjectObj;
+
+        LS.setProjectLS(this.getData());
       },
     }
+
+
+    //Local Storage
+    const getLocalStorage = () => {
+      //if Local Storage is null, set default to-dos, projects.
+      if(LS.getTodoLS() == null || LS.getProjectLS() == null){
+        //set default To-dos
+        LS.todoLS = LS.defaultTodo;
+        todoModel.todoObj.push(...LS.todoLS);
+        LS.setTodoLS(LS.todoLS);
+
+        //set default projects
+        LS.projectLS = LS.defaultProject;
+        projectModel.projectObj.push(...LS.projectLS);
+        LS.setProjectLS(LS.projectLS);
+      }else {
+        //if Local Storage is not empty, get data and set them
+        LS.todoLS = LS.getTodoLS();
+        todoModel.todoObj.push(...LS.todoLS);
+
+        LS.projectLS = LS.getProjectLS();
+        projectModel.projectObj.push(...LS.projectLS);
+      }
+    }
+
+    //first rendering
+    getLocalStorage();
+
 
     return {
       todoModel,
